87c83167f2ecdc76d4c7dcfe69d6a2a6ed48a0af
Brian Done <briguy2078@gmail.com>
2021-12-28 14:32:32 -0500

    add Heroku

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
+const { notes } = require('./data/notes');
+
+// Include Express
 const express = require('express');
 
+// Instantiate the Server
 const app = express();
 
-const { notes } = require('./data/db');
-console.log(notes);
-
+// Get PORT
 const PORT = process.env.PORT || 3001;
 
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  if (query.text) {
+    filteredResults = filteredResults.filter(note => note.text === query.text);
+  }
+  return filteredResults;
+}
 
 
 
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
 
 
